614257a36e395a655c151b8a3a9cfd80d3d96891
Kenia de Oliveira <keniapubg@gmail.com>
2023-10-03 14:57:35 -0300

    Finalizando a lista

--- Lista.c
+++ Lista.c
@@ -1,74 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 5
+#define TAM 6 //define o tamanho da lista
 
-int lista[TAM], inicio = 0, fim = 0;
+int lista[TAM], cont = 1; //declara a lista e a variável do contador
 
-void adicionar(int valor){
-    int resposta, valor_removido, escolha;
+void adicionar(int valor, int posicao){//declara a funcao adicionar que tem as variaveis 'valor' e 'posição' como parametros
 
-    if(fim  == (inicio + (TAM -1) % TAM)) {
-        printf("Lista cheia!\nDeseja remover um valor? [1]SIM  [2]NAO");
-        scanf("%d", &resposta);
-        
-        if(resposta == 1){
-            mostrar_lista();
+    if (posicao < 1 || posicao > 7){//verifica se a posição informada é valida
+        printf("\nPosicao invalida!\n");
     
-            printf("Qual valor voce quer remover? ");
-            scanf("%d", &escolha);
+    }else if(cont == TAM){
+        printf("\nA lista esta cheia!\n");
+    }
+    else{
     
-            valor_removido = remover(escolha);
-            printf("O valor %d foi removido!", valor_removido);
-    
-        }else{
+        while (lista[posicao] != 0){
+            
+            posicao = (posicao + 1) % TAM;
+        }
+        lista[posicao] = valor;
+        printf("Valor adicionado na posicao %d\n", posicao);
+        cont++;
+    }
+}
+
+int remover(int posicao){
+    for (int i = 1; i < TAM; i++)
+    {
+        if (lista[i] == lista[posicao]){
+            lista[i] = 0;
             return;
         }
     }
-
-    lista[TAM] = valor;
-
 }
-void remover(){
 
-}
 
 int mostrar_lista(){
-
+    int vazio = 0;
+    for (int i = 1; i < TAM; i++){
+        if (lista[i] == 0) {
+            printf("0 ");
+            vazio++;
+        }
+        else{
+            printf("%d ", lista[i]);
+        }
+    }
+    
+    if (vazio == 5){
+        printf("\nLista vazia!\n");
+    }
+    
+    printf("\n");
 }
 
 int main(){
-    mostrar_lista();
    
     while(1){
 
-        int opcao, valor;
-        printf("== LISTA ==\n[1]Adicionar\n[2]Remover\n[0]Sair");
+        mostrar_lista();
+        int opcao, valor, posicao;
+        printf("== LISTA ==\n[1]Adicionar\n[2]Remover\n[0]Sair\n");
         scanf("%d", &opcao);
 
         if (opcao == 1){
             
             printf("Digite um valor para adicionar: ");
             scanf("%d", &valor);
-        
-            adicionar(valor);
+
+            printf("Digite a posicao: ");
+            scanf("%d", &posicao);
+
+            adicionar(valor, posicao);
 
         }else if (opcao == 2)
         {
             mostrar_lista();
         
-            printf("Digite um valor para remover: ");
-            scanf("%d", &valor);
+            printf("Qual a posicao que o valor esta? ");
+            scanf("%d", &posicao);
         
-            remover(valor);
-            printf("Valor removido!");
+            remover(posicao);
+            printf("\nValor removido!\n");
 
         }else{
             break;
         }
-        
-        
     }
-        
-    }
-    
+}
